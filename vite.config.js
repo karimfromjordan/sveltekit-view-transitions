173843f45a4d0603101138cce167263007481fc0
Karim Jordan <karimfromjordan@gmail.com>
2023-06-14 03:35:11 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    fs: {
-      allow: ["kit"],
-    },
-  },
+	plugins: [sveltekit()]
 });
